d70a441cd904a21453eac17ad77e8c6d7cc776c7
Lucas-de-Andrade <66327882+Lucas-de-Andrade@users.noreply.github.com>
2020-06-20 23:28:36 -0300

    Add files via upload

---
+++ Exerc3.dart
@@ -0,0 +1,26 @@
+importar  'dardo: io' ;
+
+vazio  principal ()
+{
+duplo nota1, nota2;
+  mídia dupla;
+  
+  media = (nota1 + nota2) /  2 ;
+  
+  print ( "Digite a sua nota 1:" );
+  
+  print ( "Digite a sua nota 2:" );
+  
+  print ( "A média das notas são: $ media " );
+  
+  if ( mídia < 5 )
+    print ( "Reprovado" );
+    
+     caso contrário, se (mídia > = 5 ) e & ( mídia < 7 )
+      print ( "Recuperação" );
+  
+  outro 
+    print ( "Aprovado" );
+  
+     
+}
---
+++ Exerc4.dart
@@ -0,0 +1,18 @@
+importar  'dardo: io' ;
+
+vazio  principal ()
+{
+  altura dupla, base, área;
+  
+  print ( "Digite uma altura para o triângulo:" );
+  
+  altura =  .parse duplo (stdin.readLineSync ());
+  
+  print ( "Digite uma base para o triânguçp:" );
+  
+  base =  .parse duplo (stdin.readLineSync ());
+  
+  área = (base * altura) /  2 ;
+  
+  print ( "Área é = $ área " );
+}
